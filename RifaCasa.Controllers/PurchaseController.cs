d21efa691ff20bf1d046ce17280c5e26953c1df8
João Víctor DS <diassouzajoao2@gmail.com>
2025-05-22 20:52:01 -0400

    Criando o controller PurchaseController

---
+++ RifaCasa.Controllers/PurchaseController.cs
@@ -0,0 +1,18 @@
+﻿using RifaCasa.Services.Raffle;
+using RifaCasa.Services.Buyer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RifaCasa.Controllers
+{
+    public class PurchaseController(IRaffleService raffleService, IBuyerService buyerService) : Controller
+    {
+        private readonly IRaffleService _raffleService = raffleService;
+        private readonly IBuyerService _buyerService = buyerService;
+
+        [HttpPost]
+        public async Task BuyRafflesAsync()
+        {
+
+        }
+    }
+}
