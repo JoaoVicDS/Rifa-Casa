d92d42029e1b6d81333854adb121e2866c4079e9
JoaoVicDS <diassouzajoao2@gmail.com>
2025-05-23 13:38:06 -0400

    Começando a criar um Model da tabela Payment

---
+++ RifaCasa.Data/Models/Payment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RifaCasa.Data.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Phone")]
+        [Required(ErrorMessage = "Telefone do Comprador é obrigatório")]
+        public required string BuyerPhone { get; set; }
+        public Buyer? Buyer { get; set; }
+    }
+}
